377398e0a8ddf711a29ae83efd3b0c436279f897
Jesse Brown <brownjesse8895@yahoo.com>
2020-12-29 00:13:26 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -27,8 +27,9 @@ def using_shift(array)
 end
 
 
+
 def using_shift(array)
-  ice_cream_brands = ["Blue Bell Creameries", "Ben & Jerry's", "Baskin Robbins", "Braum's", "Breyer's"]
+
   array.shift
 end
 
